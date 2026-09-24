cdd0e30461ad4bdbd995b1a532b2a3a92ab62fe5
buryfelix <felix.lemieux2000@hotmail.com>
2026-09-24 14:54:44 -0400

    Création des attributs, constructeurs, getters et méthodes estCorrect & lettreA dans classe Question.js

--- js/classes/Question.js
+++ js/classes/Question.js
@@ -4,6 +4,10 @@
  */
 class Question {
 
+    #enonce;
+    #options;
+    #indexCorrect;
+
     /**
      * @param {Object} data - Données de la question
      * @param {string} data.question - L'intitulé de la question
@@ -11,7 +15,26 @@ class Question {
      * @param {number} data.correct - Index de la bonne réponse (0..3)
      */
     constructor({question, options, correct}) {
+        this.#enonce = question;
+        this.#options = options;
+        this.#indexCorrect = correct;
+    }
 
+
+    get enonce() {
+        return this.#enonce;
+    }
+
+    get options() {
+        return this.#options;
+    }
+
+    get indexCorrect() {
+        return this.#indexCorrect;
+    }
+
+    estCorrect(index) {
+        return index === this.indexCorrect;
     }
 
     /**
@@ -20,5 +43,21 @@ class Question {
      * @returns {string}
      */
     lettreA(index) {
+        let correspondant;
+        switch (index) {
+            case 0:
+                correspondant = 'A';
+                break;
+            case 1:
+                correspondant = 'B';
+                break;
+            case 2:
+                correspondant = 'C';
+                break;
+            case 3:
+                correspondant = 'D';
+                break;
+        }
+        return correspondant;
     }
 }
